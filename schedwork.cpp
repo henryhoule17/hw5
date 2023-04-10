6312f4987d65aa819997718ba77c98fbc15103ef
Henry Houle <hhoule@usc.edu>
2023-04-10 00:30:41 +0000

    Finished

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,7 +21,8 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
-
+bool scheduleHelper(DailySchedule& sched, const AvailabilityMatrix& avail, const size_t dailyNeed, const size_t maxShifts, unsigned int row, unsigned int col, std::vector<int>& worker_shifts);
+bool notAlreadyScheduled(const DailySchedule& sched, Worker_T id, unsigned int row);
 
 // Add your implementation of schedule() and other helper functions here
 
@@ -38,8 +39,71 @@ bool schedule(
     sched.clear();
     // Add your code below
 
+    //initializes sched with the proper dimensions 
+    //and is populated with INVALID_ID
+    vector<Worker_T> day_sched;
+	for(size_t i =0; i < avail.size(); i++)
+    {
+        sched.push_back(day_sched);
+        for(size_t j = 0; j < dailyNeed; j++)
+            sched[i].push_back(INVALID_ID);
+    }
 
+    //initializes vector to track how many shifts 
+    //each worker has
+    vector<int> worker_shifts;
+    for(size_t i=0; i<avail[i].size(); i++)
+        worker_shifts.push_back(0);
 
-
+    return scheduleHelper(sched, avail, dailyNeed, maxShifts, 0, 0, worker_shifts);
 }
 
+bool scheduleHelper(DailySchedule& sched, 
+                    const AvailabilityMatrix& avail, 
+                    const size_t d, 
+                    const size_t m, 
+                    unsigned int row, 
+                    unsigned int col, 
+                    std::vector<int>& worker_shifts)
+{
+    //if schedule is complete
+    if (row == sched.size() - 1 && col == d)
+      return true; 
+    
+    //if at end of column
+    if (col == d)
+    {
+      row++;
+      col = 0;
+    } 
+
+    //if shift empty, find first worker that can fill int
+    for(size_t i=0; i<avail[row].size(); i++)
+    {
+        //cout << "Row: " << row << "  Col: " << col << endl;
+        if(avail[row][i] == 1 && notAlreadyScheduled(sched, i, row) && worker_shifts[i] < m)
+        {
+            //cout << "Adding " << i << endl;
+            worker_shifts[i]++;
+            sched[row][col] = i;
+            bool nextSlot = scheduleHelper(sched, avail, d, m, row, col + 1, worker_shifts);
+            if(nextSlot) 
+                return true;
+            sched[row][col] = INVALID_ID;
+            worker_shifts[i]--;
+            //cout << "Unadding " << i << endl;
+        }
+    }
+
+	return false;
+}
+
+bool notAlreadyScheduled(const DailySchedule& sched, Worker_T id, unsigned int row)
+{
+    for(unsigned int i=0; i<sched[row].size(); i++)
+    {
+        if(sched[row][i] == id)
+            return false;
+    }
+    return true;
+}
--- wordle.cpp
+++ wordle.cpp
@@ -11,18 +11,63 @@
 #include "dict-eng.h"
 using namespace std;
 
-
 // Add prototypes of helper functions here
-
+void wordleHelper(std::string in, const std::string& floating, const std::set<std::string>& dict, std::set<std::string>& results, unsigned int position);
+//bool hasFloats(std::string in, std::string floating);
+bool isValid(std::string in, const std::string& floating);
 
 // Definition of primary wordle function
-std::set<std::string> wordle(
-    const std::string& in,
-    const std::string& floating,
-    const std::set<std::string>& dict)
+std::set<std::string> wordle(const std::string& in, const std::string& floating, const std::set<std::string>& dict)
 {
     // Add your code here
+    std::set<std::string> results;
+    std::string incopy = in;
+    std::string fcopy = floating;
 
+    wordleHelper(incopy, fcopy, dict, results, 0);
+    return results;
 }
 
 // Define any helper functions here
+void wordleHelper(std::string in, const std::string& floating, const std::set<std::string>& dict, std::set<std::string>& results, unsigned int position)
+{
+    if(!isValid(in, floating))
+        return;
+
+    if(position == in.length())
+    {
+        if(dict.find(in) != dict.end() && isValid(in, floating))
+            results.insert(in);
+
+        return;
+    }
+    if(in[position] == '-')
+    {
+        for(char c='a'; c <= 'z'; c++)
+        {
+            in[position] = c;
+            wordleHelper(in, floating, dict, results, position+1);
+        }
+    }
+    else
+        wordleHelper(in, floating, dict, results, position+1);
+}
+
+bool isValid(std::string in, const std::string& floating)
+{
+    if(floating == "")
+        return true;
+    for(unsigned int i=0; i<floating.length(); i++)
+    { 
+        if(in.find(floating[i]) == std::string::npos && in.find('-') == std::string::npos)
+            return false;
+        else
+        {
+            if(in.find(floating[i]) != std::string::npos)
+                in.erase(in.find(floating[i]), 1);
+            else
+                in.erase(in.find('-'), 1);
+        }
+    }
+    return true;
+}
